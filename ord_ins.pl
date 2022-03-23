07f285cce0f3f970153e753e5dcec4c99dbb8aa6
jcarpio <jose.carpio@gmail.com>
2022-03-23 14:32:43 +0100

    Create ord_ins.pl

---
+++ ord_ins.pl
@@ -0,0 +1,45 @@
+
+
+/*
+
+ord_ins(+Lista, -R)
+  es cierto si R unifica con una lista
+  que contiene los elementos de Lista
+  ordenados de menor a mayor.
+  
+  
+  + : la variable tiene que estar instanciada
+  - : la variable tiene que estar libre
+  ? : la variable puede estar libre o instanciada
+
+*/
+
+
+ord_ins([], []).
+
+ord_ins([Cab|Resto], R2):-
+  ord_ins(Resto, R),
+  insertar_ord(Cab, R, R2).
+  
+  
+/*  
+  
+insertar_ord(+E, +Lista, -R)
+  es cierto si R unifica con una lista
+  que contiene los elementos de Lista
+  (es una lista ordenada), con E insertado
+  en la posición correcta.  
+  
+*/  
+
+insertar_ord(E, [], [E]).
+
+insertar_ord(E, [Cab|Resto],[Cab|R]):-
+   E > Cab,
+   insertar_ord(E,Resto, R).
+   
+   
+   
+   
+insertar_ord(E, [Cab|Resto],[E,Cab|Resto]):-
+  E =< Cab.
