d0e73cba9bc234fe184d6d6219739f27f969f2af
jcarpio <jose.carpio@gmail.com>
2022-03-29 10:29:44 +0200

    Create primos_xy.pl

---
+++ primos_xy.pl
@@ -0,0 +1,5 @@
+% lista_divisores(+X, +Y, -ListaR).
+% es cierto cuando ListaR unifica con una lista
+% que contiene a los números cuyo resto
+% de la división entera de X entre Z es igual a 0
+% para valores de Z entre 1 e Y.
