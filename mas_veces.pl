4fda060513cd5378664fbf12455380b4cb76fe4c
jcarpio <jose.carpio@gmail.com>
2022-03-30 14:19:20 +0200

    Create mas_veces.pl

---
+++ mas_veces.pl
@@ -0,0 +1,21 @@
+/*
+
+comprime(+Lista, -R)
+ es cierto si R unifica con una lista
+ de la siguiente forma:
+ comprime([a,a,b,b,b,c,d,d], R).
+ R= [(a,2), (b,3), (c,1), (d,2)]
+
+*/
+
+
+
+/*
+
+mas_veces(+Lista, -Elem, -N)
+  es cierto si Elem unifica con el número
+  que se repite más veces en Lista y N
+  unifica con el número de veces que se
+  repite.  
+
+*/
