70476ec2358f2d74757336f6f0780530a99a82f4
jcarpio <jose.carpio@gmail.com>
2022-04-20 14:34:41 +0200

    Create balanceado.pl

---
+++ balanceado.pl
@@ -0,0 +1,28 @@
+
+/*
+
+ balanceado(+Arbol)
+   es cierto si para todo nodo de Árbol
+   la diferencia entre la altura del sub-arbol
+   izquierdo y el sub-arbol derecho es como
+   máximo 1.
+
+*/
+
+
+/*
+
+altura(+Arbol, -Alt)
+  es cierto si Alt unifica con la altura de Arbol.
+  
+     1
+    / \   
+   2   3
+      / \
+     4   5
+	      \
+		   6 
+		   
+   Atura = 3
+  
+*/
