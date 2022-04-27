008120f955e41d5d05fb067ba2ef4ee089cd95e4
jcarpio <jose.carpio@gmail.com>
2022-04-27 14:35:22 +0200

    Create crea_arbol_binario.pl

---
+++ crea_arbol_binario.pl
@@ -0,0 +1,24 @@
+
+/*
+
+  crea_arbol(+I, +N, -Arbol)
+  es cierto si Arbol unifica con un árbol
+  binario de N nodos con valores desde 
+  I hasta N.
+  
+*/
+
+  crea_arbol(I, N, a(N,nil,nil) ):-
+    1 is N - I.
+	
+  crea_arbol(I, N, a(I2, a(N, nil, nil),nil) ):-
+    2 is N - I,
+	I2 is I+1.
+	 
+  crea_arbol(I, N, a(IMed2, HI, HD)):-
+    Int is N - I,
+	Int > 2,
+	IMed is Int div 2,
+	crea_arbol(I, IMed, HI),
+	IMed2 is IMed + 1,
+	crea_arbol(IMed2, N, HD).
