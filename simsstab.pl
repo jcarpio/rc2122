245660f9cd097d66891c9b97dda42925f96cc9e1
jcarpio <jose.carpio@gmail.com>
2022-05-10 10:31:06 +0200

    Create simsstab.pl

---
+++ simsstab.pl
@@ -0,0 +1,16 @@
+:- load_files('req.pl').
+
+classes(Classes) :-
+       setof(C, S^N^T^class_subject_teacher_times(C,S,T,N), Classes).
+		
+teachers(Teachers) :-
+        setof(T, C^S^N^class_subject_teacher_times(C,S,T,N), Teachers).
+		
+		
+requirements(Rs) :-
+        Goal = class_subject_teacher_times(Class,Subject,Teacher,Number),
+        setof(req(Class,Subject,Teacher,Number), Goal, Rs0),
+        maplist(req_with_slots, Rs0, Rs).
+
+req_with_slots(R, R-Slots) :- R = req(_,_,_,N), length(Slots, N).		
+		
