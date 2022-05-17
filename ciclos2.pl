32ef287d2341f068a3173f23bc69e33cbf4354d7
jcarpio <jose.carpio@gmail.com>
2022-05-17 10:30:28 +0200

    Create ciclos2.pl

---
+++ ciclos2.pl
@@ -0,0 +1,40 @@
+/*
+
+camino(+Inicio, +Fin, +Grafo, +Visitados, -Camino)
+  es cierto si Camino unifica con una lista
+  de aristas/vértices que va desde Inicio hasta
+  Fin sin repetir las aristas/vértices de la lista
+  Visitados.
+  
+*/
+
+camino(Ini, Fin, g(_,A), _, [Ini,Fin]):- conectado(Ini, Fin, A). 
+
+camino(Ini, Fin, g(_,A), Visitados, [Ini|Camino]):-
+  conectado(Ini, TMP, A), 
+  \+ member(TMP, Visitados),
+  camino(TMP, Fin, g(_, A), [TMP|Visitados], Camino).
+  
+/*
+conectado(+Ini, +Fin, +Aristas)  
+  es cierto si el punto Ini y el punto Fin estan
+  conectados por una arista de la lista Aristas.
+
+*/
+    
+conectado(Ini, Fin, Aristas):- member(arista(Ini, Fin), Aristas).
+conectado(Ini, Fin, Aristas):- member(arista(Fin, Ini), Aristas).
+	
+
+grafo1(g([a,b,c,d,e], [arista(a,b), arista(a,c), arista(b,c),
+                       arista(b,d), arista(d,e), arista(e,c),
+					   arista(b,e), arista(d,c)])).
+
+
+/*
+
+grafo1(g(_,A)), camino(a, a, g(_,A), [a], Camino), length(Camino, Length), Length > 3.
+
+ grafo1(g(V,A)), member(N, V), setof(Camino, camino(N, N, g(_,A), [], Camino), R).
+ 
+*/
