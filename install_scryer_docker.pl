499ec38ba2b390e1bb4233861e3dde64ecc06d9e
jcarpio <jose.carpio@gmail.com>
2022-06-01 13:44:13 +0100

    Create install_scryer_docker.pl

---
+++ install_scryer_docker.pl
@@ -0,0 +1,36 @@
+1. Lauch docker Ubuntu 18.04 
+
+docker run -it ubuntu:18.04 /bin/bash
+
+
+2. Install apt-utils
+
+sudo apt update
+sudo apt install apt-utils 
+
+
+3. Install curl
+sudo apt install curl
+
+4. Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+5. Install Git
+apt install git
+
+6. Install GCC
+apt install build-essential
+
+6. Install m4  
+apt-get install m4
+
+7. Install nano
+apt install nano
+
+8. Copy sudoku.pl  
+git clone https://github.com/triska/clpz.git
+
+9. Install Scryer
+$> git clone https://github.com/mthom/scryer-prolog
+$> cd scryer-prolog
+$> cargo run [--release]
